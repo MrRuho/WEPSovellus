2926f14f5708a35e6dc56bdda0c2439f90f89e1f
MrRuho <paajanen.ville@gmail.com>
2023-09-23 14:14:44 +0300

    Updated SQL schema

--- schema.sql
+++ schema.sql
@@ -13,8 +13,17 @@ CREATE TABLE topic (
     header TEXT,
     content TEXT,
     sender TEXT,
-    tag TEXT
+    tag TEXT,
+    score INT
 );
+
+INSERT INTO topic (header, content, sender, tag, score) VALUES ('Slava ukraini! Foorumin eka!', 'slava ukraini!', 'Tero', 'Sota', 130);
+INSERT INTO topic (header, content, sender, tag, score) VALUES ('Seppo takas!', 'Ei tuosta kepin heittelystä tule yhtään mitään.', 'Tuhnu66', 'Urheilu', 80);
+INSERT INTO topic (header, content, sender, tag, score) VALUES ('Esittävää taidetta kiitos!', 'Ketä kiinnostaa jotkut epämääriset kakkakipot', 'Summy', 'Tiede', 88 );
+INSERT INTO topic (header, content, sender, tag, score) VALUES ('Putinin tee resepti', 'Kiehauta vesi. Kun vesi on 110c niin lisää minttu ja polonium. Anna hautua', 'Tero', 'Sota', 104);
+INSERT INTO topic (header, content, sender, tag, score) VALUES ('RTX2090!', 'Nyt muuten pyörii doom!!!', 'Joku_Vaa', 'Teknologia', 98);
+INSERT INTO topic (header, content, sender, tag, score) VALUES ('Piirakka resepti?', 'Tietääkö joku hyvää piirakka reseptiä?', 'Saarx88', 'Ruoka', 100);
+
 
 CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
@@ -23,3 +32,12 @@ CREATE TABLE messages (
     topic_id INT
 );
 
+INSERT INTO messages (content, sender, topic_id)
+VALUES
+  ('Slava Ukraini!', 'Tero', 1),
+  ('Slava Ukraini!', 'Tuhnu66', 1),
+  ('Slava Ukraini!', 'Summy', 1),
+  ('Slava Ukraini!', 'Tero', 1),
+  ('Slava Ukraini!', 'Joku_Vaa', 1),
+  ('Slava Ukraini!', 'Saarx88', 1),
+  ('Täytyypä testata =)', 'Saarx88',4);
